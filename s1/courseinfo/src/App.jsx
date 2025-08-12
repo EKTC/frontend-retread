cc66ed97ed189e456230a291354e3df83f631700
Ethan Cao <ethancao@y7mail.com>
2025-08-12 20:18:57 +1000

    ex 1.4

--- s1/courseinfo/src/App.jsx
+++ s1/courseinfo/src/App.jsx
@@ -34,26 +34,28 @@ const Total = (props) => {
 };
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course_name={course}></Header>
-      <Content content={[part1, part2, part3]}></Content>
+      <Content content={parts}></Content>
 
       <Total
-        total={part1.exercises + part2.exercises + part3.exercises}
+        total={parts[0].exercises + parts[1].exercises + parts[2].exercises}
       ></Total>
     </div>
   );
